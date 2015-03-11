f11b6bf4c52ab2cbe6998fbbf25719e6adcbf80e
Bruno Rocha <brurago@gmail.com>
2015-03-11 19:20:37 -0300

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+'use strict';
+// generated on 2015-02-25 using generator-gulp-bootstrap 0.0.4
+
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var gutil = require('gulp-util');
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+gulp.task('templates', function() {
+    var YOUR_LOCALS = {};
+
+    gulp.src('./app/interfaces/*.jade')
+    .pipe(jade({
+        locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./app/'))
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.sass({errLogToConsole: true}))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('app/styles'))
+        .pipe(gulp.dest('dist/styles'))
+        .pipe(reload({stream:true}))
+        .pipe($.size())
+        .pipe($.notify("Compilation complete."));;
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe(gulp.dest('dist/scripts'))
+        .pipe($.size());
+});
+
+gulp.task('html', ['styles', 'scripts'], function () {
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets())
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(reload({stream:true, once:true}))
+        .pipe($.size());
+});
+
+gulp.task('fonts', function () {
+    var streamqueue = require('streamqueue');
+    return streamqueue({objectMode: true},
+        $.bowerFiles(),
+        gulp.src('app/fonts/**/*')
+    )
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe($.size());
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['app/styles/main.css', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', [ 'templates', 'images', 'fonts', 'styles', 'scripts']);
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('serve', ['html'], function () {
+    browserSync.init(null, {
+        server: {
+            baseDir: 'app',
+            directory: true
+        },
+        debugInfo: false,
+        open: false,
+        hostnameSuffix: ".xip.io"
+    }, function (err, bs) {
+        require('opn')(bs.options.url);
+        console.log('Started connect web server on ' + bs.options.url);
+    });
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            directory: 'app/bower_components'
+        }))
+        .pipe(gulp.dest('app/styles'));
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            exclude: ['bootstrap-sass-official']
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', ['serve'], function () {
+ 
+    // watch for changes
+    gulp.watch(['app/*.html'], reload);
+ 
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('app/interfaces/*.jade', ['templates']);
+    gulp.watch('app/interfaces/**/*.jade', ['templates']);
+    gulp.watch('bower.json', ['wiredep']);
+});
